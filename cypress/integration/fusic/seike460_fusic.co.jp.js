8aaeac6bc5138e3500ad3e55bc7eaf87ad60fdef
seike460 <seike460@gmail.com>
2018-09-12 17:26:47 +0900

    :green_heart: 滑りそうなので修正

--- cypress/integration/fusic/seike460_fusic.co.jp.js
+++ cypress/integration/fusic/seike460_fusic.co.jp.js
@@ -1,10 +1,15 @@
-describe('清家史郎クビになってない？', function() {
+// 清家史郎存在チェック
+describe('清家史郎削除されてない？', function() {
   it('Visit Fusic', function() {
+
+    // Fusic サイトを開く
     cy.visit('https://fusic.co.jp/')
 
+    // 対象のエレメントを取得して、Clickして遷移
     cy.get('.staff_box')
       .click()
 
+    // HTML内に清家史郎が含まれるかをチェック
     cy.contains("清家 史郎")
   })
 })
